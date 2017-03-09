8953381aa6e6f09fc5a4f3e43f5ae997eaf4b57c
Jingming Niu <niu@jingming.ca>
2017-03-09 13:38:07 -0800

    Add C# docs for 5.1.0 and 5.1.1-alpha1

---
+++ twilio-csharp/5.1.0/search/functions_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['hangup',['Hangup',['../class_twilio_1_1_twi_m_l_1_1_voice_response.html#a7ee9bc438c662b045c7a51c3463aa793',1,'Twilio::TwiML::VoiceResponse']]],
+  ['hasnextpage',['HasNextPage',['../class_twilio_1_1_base_1_1_page.html#a8727f3ef7b7c66ff74bf3244f4b2b994',1,'Twilio::Base::Page']]],
+  ['httpmethod',['HttpMethod',['../class_twilio_1_1_http_1_1_http_method.html#aa012c795ca19e874ba0f18167a36b295',1,'Twilio::Http::HttpMethod']]]
+];
