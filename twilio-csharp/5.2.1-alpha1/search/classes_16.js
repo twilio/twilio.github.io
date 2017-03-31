6cb36b55bcb2eb81e05ecef5175386562f6474b3
Jingming Niu <niu@jingming.ca>
2017-03-31 13:32:34 -0700

    Add C# docs for 5.2.0 and 5.2.1-alpha1

---
+++ twilio-csharp/5.2.1-alpha1/search/classes_16.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['yearlyresource',['YearlyResource',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_usage_1_1_record_1_1_yearly_resource.html',1,'Twilio::Rest::Api::V2010::Account::Usage::Record']]],
+  ['yesterdayresource',['YesterdayResource',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_usage_1_1_record_1_1_yesterday_resource.html',1,'Twilio::Rest::Api::V2010::Account::Usage::Record']]]
+];
---
+++ twilio-csharp/5.2.1-alpha1/search/classes_f.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['qualityenum',['QualityEnum',['../class_twilio_1_1_rest_1_1_fax_1_1_v1_1_1_fax_resource_1_1_quality_enum.html',1,'Twilio::Rest::Fax::V1::FaxResource']]],
+  ['queryfromboundtypeenum',['QueryFromBoundTypeEnum',['../class_twilio_1_1_rest_1_1_preview_1_1_sync_1_1_service_1_1_sync_list_1_1_sync_list_item_resource_1_1_query_from_bound_type_enum.html',1,'Twilio.Rest.Preview.Sync.Service.SyncList.SyncListItemResource.QueryFromBoundTypeEnum'],['../class_twilio_1_1_rest_1_1_preview_1_1_sync_1_1_service_1_1_sync_map_1_1_sync_map_item_resource_1_1_query_from_bound_type_enum.html',1,'Twilio.Rest.Preview.Sync.Service.SyncMap.SyncMapItemResource.QueryFromBoundTypeEnum']]],
+  ['queryresultorderenum',['QueryResultOrderEnum',['../class_twilio_1_1_rest_1_1_preview_1_1_sync_1_1_service_1_1_sync_list_1_1_sync_list_item_resource_1_1_query_result_order_enum.html',1,'Twilio.Rest.Preview.Sync.Service.SyncList.SyncListItemResource.QueryResultOrderEnum'],['../class_twilio_1_1_rest_1_1_preview_1_1_sync_1_1_service_1_1_sync_map_1_1_sync_map_item_resource_1_1_query_result_order_enum.html',1,'Twilio.Rest.Preview.Sync.Service.SyncMap.SyncMapItemResource.QueryResultOrderEnum']]],
+  ['queueorderenum',['QueueOrderEnum',['../class_twilio_1_1_rest_1_1_taskrouter_1_1_v1_1_1_workspace_resource_1_1_queue_order_enum.html',1,'Twilio::Rest::Taskrouter::V1::WorkspaceResource']]],
+  ['queueresource',['QueueResource',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_queue_resource.html',1,'Twilio::Rest::Api::V2010::Account']]]
+];
