2fccf9d48bb86c67d38c8d137f3101675d0cf705
Evan Fossier <evan.fossier@gmail.com>
2017-04-13 11:37:34 -0700

    Updated C# Docs with version 5.2.1

---
+++ twilio-csharp/5.2.1/search/classes_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['httpclient',['HttpClient',['../class_twilio_1_1_http_1_1_http_client.html',1,'Twilio::Http']]],
+  ['httpmethod',['HttpMethod',['../class_twilio_1_1_http_1_1_http_method.html',1,'Twilio::Http']]],
+  ['httpmethodconverter',['HttpMethodConverter',['../class_twilio_1_1_converters_1_1_http_method_converter.html',1,'Twilio::Converters']]]
+];
---
+++ twilio-csharp/5.2.1/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mobile',['Mobile',['../class_twilio_1_1_types_1_1_inbound_call_price_1_1_type_enum.html#a24d64c1ffe649ea8b42bd4cb9a723b3c',1,'Twilio.Types.InboundCallPrice.TypeEnum.Mobile()'],['../class_twilio_1_1_types_1_1_inbound_sms_price_1_1_type_enum.html#aa740553ad3cb48dbc37b4b5a80f49dd5',1,'Twilio.Types.InboundSmsPrice.TypeEnum.Mobile()'],['../class_twilio_1_1_types_1_1_phone_number_price_1_1_type_enum.html#a3869d2e610e048da7cbaae5b678f6e3b',1,'Twilio.Types.PhoneNumberPrice.TypeEnum.Mobile()']]]
+];
