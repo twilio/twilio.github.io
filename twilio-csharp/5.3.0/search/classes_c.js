a3e369f506f87278b8dae1365419e6094d6729f0
Jingming Niu <niu@jingming.ca>
2017-04-17 15:18:03 -0700

    Updated C# Docs with version 5.3.0

---
+++ twilio-csharp/5.3.0/search/classes_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['newkeyresource',['NewKeyResource',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_new_key_resource.html',1,'Twilio::Rest::Api::V2010::Account']]],
+  ['newsigningkeyresource',['NewSigningKeyResource',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_new_signing_key_resource.html',1,'Twilio::Rest::Api::V2010::Account']]],
+  ['notificationresource',['NotificationResource',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_notification_resource.html',1,'Twilio.Rest.Api.V2010.Account.NotificationResource'],['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_call_1_1_notification_resource.html',1,'Twilio.Rest.Api.V2010.Account.Call.NotificationResource']]],
+  ['numberresource',['NumberResource',['../class_twilio_1_1_rest_1_1_pricing_1_1_v1_1_1_voice_1_1_number_resource.html',1,'Twilio::Rest::Pricing::V1::Voice']]]
+];
---
+++ twilio-csharp/5.3.0/search/properties_e.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['order',['Order',['../class_twilio_1_1_rest_1_1_chat_1_1_v1_1_1_service_1_1_channel_1_1_read_message_options.html#afeac8367e5df7d5d1033591e26dea91e',1,'Twilio.Rest.Chat.V1.Service.Channel.ReadMessageOptions.Order()'],['../class_twilio_1_1_rest_1_1_ip_messaging_1_1_v1_1_1_service_1_1_channel_1_1_read_message_options.html#a95cdb2b2814f863c1774c1a0645fc68b',1,'Twilio.Rest.IpMessaging.V1.Service.Channel.ReadMessageOptions.Order()']]],
+  ['ordering',['Ordering',['../class_twilio_1_1_rest_1_1_taskrouter_1_1_v1_1_1_workspace_1_1_read_task_options.html#a5d9c220c3f1853bb01976b64aac82aca',1,'Twilio::Rest::Taskrouter::V1::Workspace::ReadTaskOptions']]],
+  ['outboundcallprice',['OutboundCallPrice',['../class_twilio_1_1_rest_1_1_pricing_1_1_v1_1_1_voice_1_1_number_resource.html#a504f949b85ff9b2a6883f81766a0b21f',1,'Twilio::Rest::Pricing::V1::Voice::NumberResource']]],
+  ['outboundprefixprices',['OutboundPrefixPrices',['../class_twilio_1_1_rest_1_1_pricing_1_1_v1_1_1_voice_1_1_country_resource.html#afe21e9d816bfee5492fc24bda9015715',1,'Twilio::Rest::Pricing::V1::Voice::CountryResource']]],
+  ['outboundsmsprices',['OutboundSmsPrices',['../class_twilio_1_1_rest_1_1_pricing_1_1_v1_1_1_messaging_1_1_country_resource.html#a7d22d3d70ab6b43ff15f88ab7f4d4256',1,'Twilio::Rest::Pricing::V1::Messaging::CountryResource']]],
+  ['outcome',['Outcome',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_message_1_1_create_feedback_options.html#a4b87fe4325d4c62781fb6d29af52c6c6',1,'Twilio.Rest.Api.V2010.Account.Message.CreateFeedbackOptions.Outcome()'],['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_message_1_1_feedback_resource.html#a5f76cbd60d53a7651fccf737cf64a132',1,'Twilio.Rest.Api.V2010.Account.Message.FeedbackResource.Outcome()']]],
+  ['outgoingapplicationparams',['OutgoingApplicationParams',['../class_twilio_1_1_jwt_1_1_access_token_1_1_voice_grant.html#abfbffec69ed20e2dd29fbd618d315100',1,'Twilio::Jwt::AccessToken::VoiceGrant']]],
+  ['outgoingapplicationsid',['OutgoingApplicationSid',['../class_twilio_1_1_jwt_1_1_access_token_1_1_voice_grant.html#a9d0679e5fd77be771c52d251ccf8bd79',1,'Twilio::Jwt::AccessToken::VoiceGrant']]],
+  ['owneraccountsid',['OwnerAccountSid',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_resource.html#a9d97ab1e147b331a20dddec18d38f5db',1,'Twilio::Rest::Api::V2010::AccountResource']]]
+];
