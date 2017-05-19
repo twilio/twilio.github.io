7298fc9690b795109f40c9c01d7082e3ea6601ae
Tom Connors <tconnors@twilio.com>
2017-05-19 16:42:25 -0700

    Updated C# Docs with version 5.4.2
    Forgot TAG

---
+++ twilio-csharp/5.4.2/search/properties_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['groupingsid',['GroupingSid',['../class_twilio_1_1_rest_1_1_video_1_1_v1_1_1_read_recording_options.html#a4c174b7191bc9c3e47739e1b56de3bf3',1,'Twilio::Rest::Video::V1::ReadRecordingOptions']]],
+  ['groupingsids',['GroupingSids',['../class_twilio_1_1_rest_1_1_video_1_1_v1_1_1_recording_resource.html#a8a5996937e3068d1a1c70ebb3b17e44b',1,'Twilio.Rest.Video.V1.RecordingResource.GroupingSids()'],['../class_twilio_1_1_rest_1_1_video_1_1_v1_1_1_room_1_1_recording_resource.html#a999ffafcaeac46fae75cbcc8c54e5b0e',1,'Twilio.Rest.Video.V1.Room.RecordingResource.GroupingSids()']]],
+  ['groupsid',['GroupSid',['../class_twilio_1_1_rest_1_1_api_1_1_v2010_1_1_account_1_1_call_resource.html#af91fe53364caf1e654089326fafd3fa4',1,'Twilio::Rest::Api::V2010::Account::CallResource']]]
+];
