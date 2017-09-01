c6c70fe5c222e4f4246fcb6f4293d1fb83bd86dd
Doug Black <dblack@twilio.com>
2017-09-01 15:54:23 -0700

    Updated C# Docs with version 5.6.4

---
+++ twilio-csharp/5.6.4/html/search/classes_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['basejwt',['BaseJwt',['../class_twilio_1_1_jwt_1_1_base_jwt.html',1,'Twilio::Jwt']]],
+  ['bindingresource',['BindingResource',['../class_twilio_1_1_rest_1_1_notify_1_1_v1_1_1_service_1_1_binding_resource.html',1,'Twilio::Rest::Notify::V1::Service']]],
+  ['bindingtypeenum',['BindingTypeEnum',['../class_twilio_1_1_rest_1_1_notify_1_1_v1_1_1_service_1_1_binding_resource_1_1_binding_type_enum.html',1,'Twilio.Rest.Notify.V1.Service.BindingResource.BindingTypeEnum'],['../class_twilio_1_1_rest_1_1_notify_1_1_v1_1_1_service_1_1_user_1_1_user_binding_resource_1_1_binding_type_enum.html',1,'Twilio.Rest.Notify.V1.Service.User.UserBindingResource.BindingTypeEnum']]]
+];
---
+++ twilio-csharp/5.6.4/html/search/variables_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['required',['Required',['../class_twilio_1_1_jwt_1_1_taskrouter_1_1_policy_1_1_filter_requirement.html#a6e929ac14e8ef4c32a646a5511c0cb61',1,'Twilio::Jwt::Taskrouter::Policy::FilterRequirement']]]
+];
