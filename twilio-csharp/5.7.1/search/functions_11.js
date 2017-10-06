8965b2c180c3136f2951ada5157e9c45f4b50c5f
Jeremy McEntire <jmcentire@twilio.com>
2017-10-06 15:19:14 -0700

    Updated C# Docs with version 5.7.1

---
+++ twilio-csharp/5.7.1/search/functions_11.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['say',['Say',['../class_twilio_1_1_twi_m_l_1_1_gather.html#adbda0f81fecde0de9b076856dd974eef',1,'Twilio.TwiML.Gather.Say()'],['../class_twilio_1_1_twi_m_l_1_1_voice_response.html#a65a80b674240549d79d361ba03337d33',1,'Twilio.TwiML.VoiceResponse.Say()']]],
+  ['serialize',['Serialize',['../class_twilio_1_1_converters_1_1_prefixed_collapsible_map.html#a6d455c888b7ad72a88fd984c5edb7d07',1,'Twilio::Converters::PrefixedCollapsibleMap']]],
+  ['setaccountsid',['SetAccountSid',['../class_twilio_1_1_twilio_client.html#a4188d9b9631ab816d6ab88a48c7e2028',1,'Twilio::TwilioClient']]],
+  ['setauth',['SetAuth',['../class_twilio_1_1_http_1_1_request.html#a47e73d0d807444a871ed2ec2a2043329',1,'Twilio::Http::Request']]],
+  ['setpassword',['SetPassword',['../class_twilio_1_1_twilio_client.html#ad4a4aa58a64629ecd7d29654e35275e5',1,'Twilio::TwilioClient']]],
+  ['setrestclient',['SetRestClient',['../class_twilio_1_1_twilio_client.html#aab32076e18d28e2462e45ce63c43ac42',1,'Twilio::TwilioClient']]],
+  ['setusername',['SetUsername',['../class_twilio_1_1_twilio_client.html#a6f0e2e5f3e7d6173393d610954dbc531',1,'Twilio::TwilioClient']]],
+  ['sip',['Sip',['../class_twilio_1_1_twi_m_l_1_1_dial.html#a833e2e3179e7b9d37969a99413dc4296',1,'Twilio::TwiML::Dial']]],
+  ['sms',['Sms',['../class_twilio_1_1_twi_m_l_1_1_voice_response.html#a4641866da21fc05534ee27b0d11f21b1',1,'Twilio::TwiML::VoiceResponse']]],
+  ['stringenum',['StringEnum',['../class_twilio_1_1_types_1_1_string_enum.html#a6fb269ba42c3d925466810ae0b381d4c',1,'Twilio.Types.StringEnum.StringEnum()'],['../class_twilio_1_1_types_1_1_string_enum.html#aa40db8b8d4df8619c99877bb58a1fbd2',1,'Twilio.Types.StringEnum.StringEnum(string value)']]],
+  ['systemnethttpclient',['SystemNetHttpClient',['../class_twilio_1_1_http_1_1_system_net_http_client.html#ae277f753bd88138ef49a1db13f185484',1,'Twilio::Http::SystemNetHttpClient']]]
+];
