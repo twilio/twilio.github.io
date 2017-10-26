beb2216ed3dae9acf262bddc5b4c0a4c8ecf84ab
Evan Fossier <efossier@twilio.com>
2017-10-26 10:22:52 -0700

    Set github flavor on readme generator

--- js/language.js
+++ js/language.js
@@ -1,17 +1,12 @@
-
-
 function loadReadme() {
   var language = $('.readme').data('language');
 
   $.ajax({
-
     url: 'https://raw.githubusercontent.com/twilio/twilio-' + language + '/master/README.md'
-
   }).done(function(data) {
-
     var converter = new showdown.Converter();
+    converter.setFlavor('github');
     $('.readme')[0].innerHTML = converter.makeHtml(data);
-    
   })
 }
 
